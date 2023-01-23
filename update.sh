2435739c4548dea7b13af6493ec6b0d838f09dd6
Customer <u1367-g5g0f4cejq6u@uk2.siteground.eu>
2023-01-23 10:50:56 +0000

    Automated Commit

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Updating..."
+
+find /home/customer/git/website-static/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/https:\/\/stage.cdp.app/https:\/\/www.cdp.app/g'
+
+cd /home/customer/git/website-static/
+/usr/bin/git pull origin main
+/usr/bin/git add .
+/usr/bin/git commit -m "Automated Commit"
+/usr/bin/git push origin main --force
