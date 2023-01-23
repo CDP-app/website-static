ce7d6613f88ae05bb683d04ec19b614a35549682
Customer <u1367-g5g0f4cejq6u@uk2.siteground.eu>
2023-01-23 13:00:54 +0000

    Automated Commit

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 echo "Updating..."
 
-find /home/customer/git/website-static/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/https:\/\/stage.cdp.app/https:\/\/www.cdp.app/g'
+find /home/customer/git/website-static/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/https:\/\/stage.cdp.app/https:\/\/www.cdp.app/g' | xargs -0 sed -i 's/http:\/\/stage.cdp.app/http:\/\/www.cdp.app/g'
 
 cd /home/customer/git/website-static/
 /usr/bin/git pull origin main
